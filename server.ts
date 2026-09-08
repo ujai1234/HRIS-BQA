c848a4204bca7515afd867f3dc71216aaef4a98b
Hudzaifah <ujai757@gmail.com>
2026-09-08 09:41:25 +0700

    fix: replace Express 5 wildcard route '*' with regex /.*/ (path-to-regexp v8 breaking change)

--- server.ts
+++ server.ts
@@ -2890,7 +2890,8 @@ async function startServer() {
   } else {
     const distPath = path.join(process.cwd(), 'dist');
     app.use(express.static(distPath));
-    app.get('*', (req, res) => {
+    // Express 5 no longer supports '*' wildcard - use regex instead
+    app.get(/.*/, (req, res) => {
       res.sendFile(path.join(distPath, 'index.html'));
     });
   }
